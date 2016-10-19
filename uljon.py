474bbf12fb007754aab877ac9c30323dac1b5ae8
svalous <bluepower912@gmail.com>
2016-10-19 15:01:05 -0400

    Started setting up praw

--- uljon.py
+++ uljon.py
@@ -1,19 +1,23 @@
 # uljon.py
 from flask import Flask, render_template, redirect, request
 from celery import Celery
+import praw
 from os import listdir
 from os.path import isfile, join, dirname, abspath 
 from sys import path
 path.append(dirname(abspath(__file__)) + '/lib')
 import generator as Gen
+import secrets as s
 
 app = Flask(__name__)
 
+# put the below in a seperate file and .gitignore it
+
 @app.route('/')
 def index():
-	mypath = dirname(abspath(__file__)) + "/txt/"
-	files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-	return render_template('index.html', files=files) 
+	link_no_refresh = r.get_authorize_url('UniqueKey')
+	link_refresh = r.get_authorize_url('DifferentUniqueKey', refreshable=True)
+	return render_template('index.html', no_refresh = link_no_refresh, refresh = link_refresh) 
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -22,4 +26,18 @@ def generate():
 	gen = Gen.Generator(myfile)
 	sentence = gen.makeSentence(int(request.form['length']))
 	print(sentence)
-	return redirect('/', 302)
+	return redirect('/authorize_callback', 302)
+
+@app.route('/authorize_callback')
+def authorized():
+	mypath = dirname(abspath(__file__)) + "/txt/"
+	files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	state = request.args.get('state', '')
+	code = request.args.get('code', '')
+	info = r.get_access_information(code)
+	return render_template('authorize_callback.html', files=files)
+
+if __name__ == '__main__':
+    r = praw.Reddit('Random sentence generator by u/Uljon ver 0.1')
+	r.set_oauth_app_info(s.CLIENT_ID, s.CLIENT_SECRET, s.REDIRECT_URI)
+	app.run(debug=True, port=65010)
